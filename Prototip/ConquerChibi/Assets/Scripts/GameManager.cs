765e8e80010115700a6ead323e417dade8176e31
Betu97 <albert.mo@students.salle.url.edu>
2019-11-09 20:46:25 +0100

    Multiplayer / First Scenario / Folder Organization

---
+++ Prototip/ConquerChibi/Assets/Scripts/GameManager.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+using Photon.Pun;
+using Photon.Realtime;
+
+
+namespace Com.MyCompany.multiTest
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        public static GameManager Instance;
+        public GameObject playerPrefab;
+
+        void Start()
+        {
+            Instance = this;
+            if (PlayerManager.LocalPlayerInstance == null)
+            {
+                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+            }
+            else
+            {
+                Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+            }
+            
+        }
+
+        #region Photon Callbacks
+
+
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+                LoadArena();
+            }
+        }
+
+
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+                LoadArena();
+            }
+        }
+
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        private void OnApplicationQuit()
+        {
+            System.Threading.Thread.Sleep(2000);
+            LeaveRoom();
+        }
+
+
+        #endregion
+
+
+        #region Public Methods
+
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+
+        #endregion
+
+        #region Private Methods
+
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                PhotonNetwork.LoadLevel("Lobby");
+            }
+            else
+            {
+                PhotonNetwork.LoadLevel("AldeaYamikaze");
+            }
+            
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ Prototip/ConquerChibi/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,269 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+using System.Collections;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.MyCompany.multiTest
+{
+    /// <summary>
+    /// Player manager.
+    /// Handles fire Input and Beams.
+    /// </summary>
+    public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
+    {
+        public float Health = 1f;
+        public static GameObject LocalPlayerInstance;
+
+        [Tooltip("The Player's UI GameObject Prefab")]
+        [SerializeField]
+        public GameObject PlayerUiPrefab;
+
+        #region Private Fields
+
+        [Tooltip("The Beams GameObject to control")]
+        [SerializeField]
+        private GameObject beams;
+        //True, when the user is firing
+        bool IsFiring;
+        #endregion
+
+        #region Private Methods
+            #if UNITY_5_4_OR_NEWER
+                    void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode loadingMode)
+                    {
+                        this.CalledOnLevelWasLoaded(scene.buildIndex);
+                    }
+            #endif
+        #endregion
+
+        #region IPunObservable implementation
+
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                // We own this player: send the others our data
+                stream.SendNext(IsFiring);
+            }
+            else
+            {
+                // Network player, receive data
+                this.IsFiring = (bool)stream.ReceiveNext();
+            }
+
+            if (stream.IsWriting)
+            {
+                // We own this player: send the others our data
+                stream.SendNext(IsFiring);
+                stream.SendNext(Health);
+            }
+            else
+            {
+                // Network player, receive data
+                this.IsFiring = (bool)stream.ReceiveNext();
+                this.Health = (float)stream.ReceiveNext();
+            }
+
+        }
+
+
+        #endregion
+
+
+        #region MonoBehaviour CallBacks
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+        /// </summary>
+        void Awake()
+        {
+
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                PlayerManager.LocalPlayerInstance = this.gameObject;
+            }
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
+
+            if (beams == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> Beams Reference.", this);
+            }
+            else
+            {
+                beams.SetActive(false);
+            }
+        }
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
+        void Start()
+        {
+            CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork>();
+
+
+            if (_cameraWork != null)
+            {
+                if (photonView.IsMine)
+                {
+                    _cameraWork.OnStartFollowing();
+                }
+            }
+            else
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+            }
+
+            if (PlayerUiPrefab != null)
+            {
+                GameObject _uiGo = Instantiate(PlayerUiPrefab);
+                _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+            }
+            else
+            {
+                Debug.LogWarning("<Color=Red><a>Missing</a></Color> PlayerUiPrefab reference on player Prefab.", this);
+            }
+
+#if UNITY_5_4_OR_NEWER
+            // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+            #endif
+        }
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity on every frame.
+        /// </summary>
+        void Update()
+        {
+
+
+
+            // we only process Inputs if we are the local player
+            if (photonView.IsMine)
+            {
+
+                ProcessInputs();
+                
+                if (Health <= 0f)
+                {
+                    GameManager.Instance.LeaveRoom();
+                }
+            }
+
+            // trigger Beams active state
+            if (beams != null && IsFiring != beams.activeSelf)
+            {
+                beams.SetActive(IsFiring);
+            }
+        }
+
+        /// <summary>
+        /// MonoBehaviour method called when the Collider 'other' enters the trigger.
+        /// Affect Health of the Player if the collider is a beam
+        /// Note: when jumping and firing at the same, you'll find that the player's own beam intersects with itself
+        /// One could move the collider further away to prevent this or check if the beam belongs to the player.
+        /// </summary>
+        void OnTriggerEnter(Collider other)
+        {
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+            // We are only interested in Beamers
+            // we should be using tags but for the sake of distribution, let's simply check by name.
+            //Debug.LogWarning("hello" + SceneManager.GetActiveScene().name, this);
+            if (!other.name.Contains("Beam") || SceneManager.GetActiveScene().name.Equals("Lobby"))
+            {
+                return;
+            }
+            Health -= 0.1f;
+        }
+        /// <summary>
+        /// MonoBehaviour method called once per frame for every Collider 'other' that is touching the trigger.
+        /// We're going to affect health while the beams are touching the player
+        /// </summary>
+        /// <param name="other">Other.</param>
+        void OnTriggerStay(Collider other)
+        {
+            // we dont' do anything if we are not the local player.
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+            // We are only interested in Beamers
+            // we should be using tags but for the sake of distribution, let's simply check by name.
+            
+            if (!other.name.Contains("Beam") || SceneManager.GetActiveScene().name.Equals("Lobby"))
+            {
+                return;
+            }
+            // we slowly affect health when beam is constantly hitting us, so player has to move to prevent death.
+            Health -= 0.1f * Time.deltaTime;
+        }
+
+            #if !UNITY_5_4_OR_NEWER
+            void OnLevelWasLoaded(int level)
+            {
+                this.CalledOnLevelWasLoaded(level);
+            }
+            #endif
+
+
+        void CalledOnLevelWasLoaded(int level)
+        {
+            // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+            if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+            {
+                transform.position = new Vector3(0f, 5f, 0f);
+            }
+
+            GameObject _uiGo = Instantiate(this.PlayerUiPrefab);
+            _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+        }
+
+
+        #endregion
+
+        #region Custom
+
+        /// <summary>
+        /// Processes the inputs. Maintain a flag representing when the user is pressing Fire.
+        /// </summary>
+        void ProcessInputs()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (!IsFiring)
+                {
+                    IsFiring = true;
+                }
+            }
+            if (Input.GetButtonUp("Fire1"))
+            {
+                if (IsFiring)
+                {
+                    IsFiring = false;
+                }
+            }
+        }
+
+        #if UNITY_5_4_OR_NEWER
+                public override void OnDisable()
+                {
+                    // Always call the base to remove callbacks
+                    base.OnDisable();
+                    UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+                }
+        #endif
+
+        #endregion
+    }
+}
