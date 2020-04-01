1ac5f25d5dd9a0728a2da6e995361018d32ace90
Betu97 <albert.mo@students.salle.url.edu>
2020-04-01 20:48:48 +0200

    PrototipChange

---
+++ Prototip/ConquerChibiHD/Assets/Scripts/Terrains/LeverScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour
+{
+    
+    GameObject crate;
+
+    //Make it as a singleton
+    private static LeverScript instance;
+
+    public static LeverScript Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<LeverScript>();
+            }
+
+            return instance;
+        }
+    }
+
+    public void Start()
+    {
+        crate = GameObject.Find("Plataforma");
+        crate.GetComponent<Crate>().state = Crate.State.hide;
+    }
+
+    public void rotatePlatform()
+    {
+        crate.GetComponent<Crate>().mIsRotated = !crate.GetComponent<Crate>().mIsRotated;
+        crate.GetComponent<Animator>().SetBool("rotated", crate.GetComponent<Crate>().mIsRotated);
+    }
+
+}
